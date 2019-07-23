c9e9437ac548ec19d4df321a8c125a24c0ea0c7f
Victor Marzal <victor.marzal@sistemasgenomicos.com>
2019-07-23 15:05:30 +0200

    refactors

--- src/main/java/com/baeldung/matrix/MatrixMultiplicationBenchmarking.java
+++ src/main/java/com/baeldung/matrix/MatrixMultiplicationBenchmarking.java
@@ -1,5 +1,26 @@
 package com.baeldung.matrix;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
 public class MatrixMultiplicationBenchmarking {
 
+	public static void main(String[] args) throws RunnerException {
+		  Options opt = new OptionsBuilder()
+			      .include(MatrixMultiplicationBenchmarking.class.getSimpleName())
+			      .mode(Mode.AverageTime)
+			      .forks(2)
+			      .warmupIterations(5)
+			      .measurementIterations(10)
+			      .timeUnit(TimeUnit.MICROSECONDS)
+			      .build();
+			 
+			    new Runner(opt).run();
+		}
+	
 }
